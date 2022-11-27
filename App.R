81b2c0333dbc3a4e61f009b9d05abdb5346f38a6
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-11-27 01:04:05 -0400

    update app (#1)
    - updated the date range for the user
    - updated the map table to display the data in map format

--- App.R
+++ App.R
@@ -22,14 +22,26 @@ ui <- fluidPage(
       ##sorting the filtered data using the location
       selectInput("Location", label = "Province", choices = sort(unique(location$GEO))),
       
+      ##Adding the date range so that user can choose from which period businesses were closed
+      dateRangeInput('dateRange',
+                     label = 'Date range input: yyyy-mm-dd',
+                     start = Sys.Date() - 2, end = Sys.Date() + 2
+      ),
+      
     ),
     mainPanel(
       tabsetPanel(
         
-        #Panel1
+        #Panel1, output will be shown in Table format
         tabPanel(
           "Table",
           dataTableOutput("table")
+        ),
+        
+        #Panel 2, output will be in map format
+        tabPanel(
+          "Map", 
+          leafletOutput("map", width = "100%")
         )
       )
     )
@@ -39,10 +51,13 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  output$dateRangeText  <- renderText({
+    paste("input$dateRange is", 
+          paste(as.character(input$dateRange), collapse = " to ")
+    )
+  })
+  
 }
-
-
-
 
 
 
